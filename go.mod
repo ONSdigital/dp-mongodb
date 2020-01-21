a608978506ddc9958dbf165a40ecf4200b66e8d7
David Subiros <subiros.david@gmail.com>
2020-01-21 17:01:38 +0000

    Unified health and healthcheck files

--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/smartystreets/goconvey v1.6.4
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- health/health.go
+++ health/health.go
@@ -5,7 +5,13 @@ import (
 	"time"
 
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
+	health "github.com/ONSdigital/dp-healthcheck/healthcheck"
+	"github.com/ONSdigital/log.go/log"
+	mgo "github.com/globalsign/mgo"
 )
+
+// ServiceName mongodb
+const ServiceName = "mongodb"
 
 var (
 	healthyMessage = "mongodb is OK"
@@ -18,6 +24,40 @@ type Healthcheck = func(context.Context) (string, error)
 type CheckMongoClient struct {
 	client      Client
 	healthcheck Healthcheck
+}
+
+// Client provides a healthcheck.Client implementation for health checking the service
+type Client struct {
+	mongo       *mgo.Session
+	serviceName string
+	Check       *health.Check
+}
+
+// NewClient returns a new health check client using the given service
+func NewClient(db *mgo.Session) *Client {
+
+	// Initial Check struct
+	check := &health.Check{Name: ServiceName}
+
+	// Create Client
+	return &Client{
+		mongo:       db,
+		serviceName: ServiceName,
+		Check:       check,
+	}
+}
+
+// Healthcheck calls service to check its health status
+func (m *Client) Healthcheck(ctx context.Context) (res string, err error) {
+	s := m.mongo.Copy()
+	defer s.Close()
+	res = m.serviceName
+	err = s.Ping()
+	if err != nil {
+		log.Event(ctx, "Ping mongo", log.Error(err))
+	}
+
+	return
 }
 
 // Checker calls an api health endpoint and returns a check object to the caller
