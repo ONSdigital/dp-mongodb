1ccf6c460ee80385b96076eccbfdaf0c2306f2b0
morses-code <morsescott88@gmail.com>
2021-08-13 10:32:18 +0100

    updated script

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -2,7 +2,7 @@
 
 cwd=$(pwd)
 
-pushd $cwd/dp-mongodb
+pushd $cwd/dp-mongodb/mongodb
 #  This is required to tell memongo which binary to download, without this
 #  memongo tries to download a binary for debian which doesn't work on the container
   export MEMONGO_DOWNLOAD_URL=https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
